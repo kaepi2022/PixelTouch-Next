89783755133989a8c3d861f0781649baf4c7ef7d
みねクラ <103878816+kaepi2022@users.noreply.github.com>
2025-11-26 23:55:11 +0900

    Delete TAB-A05-BA1/1.0.0/pixel_startup.sh

--- TAB-A05-BA1/1.0.0/pixel_startup.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin
-
-/system/bin/settings put global charging_sounds_enabled 1
-/system/bin/settings put global settings_about_phone_v2 true
-/system/bin/settings put global settings_bluetooth_while_driving true
-/system/bin/settings put global settings_zone_picker_v2 true
-/system/bin/settings put global settings_systemui_theme true
