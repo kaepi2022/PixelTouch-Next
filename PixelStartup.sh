4b10f8d60d91703eb2f5a41428d709282019e104
みねクラ <103878816+kaepi2022@users.noreply.github.com>
2025-12-22 15:00:16 +0900

    Enable screenshot and development options in script
    Add settings for enabling screenshots and development options.

--- PixelStartup.sh
+++ PixelStartup.sh
@@ -5,3 +5,10 @@
 /system/bin/settings put global settings_bluetooth_while_driving true
 /system/bin/settings put global settings_zone_picker_v2 true
 /system/bin/settings put global settings_systemui_theme true
+/system/bin/settings put system allow_screen_shot 1
+
+is_enabled_dev_opt=$(/system/bin/settings get global development_settings_enabled)
+
+if [ "$is_enabled_dev_opt" = "1" ]; then
+    /system/bin/appops set com.android.chrome REQUEST_INSTALL_PACKAGES allow
+fi
